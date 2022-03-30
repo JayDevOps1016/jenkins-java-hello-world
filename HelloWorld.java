892082974e6e0bdd603ac46dbed79e99c50d8282
jaydeepvaghela_crest <jaydeep.vaghela@crestdatasys.com>
2022-03-30 11:48:10 +0530

    add file for testing SCM Polling is working or not

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public static void main(String arg[]){
         System.out.println("Hello, World");
+        System.out.println("This line add for checking Poll SCM Working or Not !");
     }
 }
